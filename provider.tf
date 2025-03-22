4f4f38443964dd39711b87c8a363fc0c358d1b88
Sam Diki <lixiondyely@gmail.com>
2025-03-22 16:42:29 +0300

    updated and deployed RDS instance using Terraform and migrating the database from EC2 to RDS via Github Action-3

--- provider.tf
+++ provider.tf
@@ -1,14 +1,20 @@
 
-# Configure the AWS Provider
+# # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.0"  # Compatible with 5.92.0
     }
   }
- 
+
+  backend "s3" {
+    bucket = "github-action-demo-rds"
+    key    = "github-action-demo.tfstate"
+    region = "us-east-1"
+  }
 }
