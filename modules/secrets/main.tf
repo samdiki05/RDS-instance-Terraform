20a864b08ab79127b8c95f62236837276b34c0af
Sam Diki <lixiondyely@gmail.com>
2025-03-22 19:49:31 +0300

    updated and deployed RDS instance using Terraform and migrating the database from EC2 to RDS via Github Action-07

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,8 +1,8 @@
-resource "aws_secretsmanager_secret" "rd_credentials" {
-  name = "rd_credentials"
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name = "db_credentials"
 }
 
 resource "aws_secretsmanager_secret_version" "secret_version" {
-  secret_id     = aws_secretsmanager_secret.rd_credentials.id
+  secret_id     = aws_secretsmanager_secret.db_credentials.id
   secret_string = jsonencode(var.password)
 }
